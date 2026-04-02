9c2cdfaf1ba614e1627386f129d2596b94966dc9
utkarsh9696-oss <utkarsh.shitole1@mitwpu.edu.in>
2026-04-02 22:48:26 +0530

    Deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://workwave-crnn.onrender.com/api"
+  baseURL: "https://workwavely.netlify.app"
 });
 
 API.interceptors.request.use((req) => {
